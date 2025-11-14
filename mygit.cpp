58d28cd4fe3d632930b1492fc103b59262df3495
Bouthayna <bouthaynaajmi5@gmail.com>
2025-11-14 16:22:43 +0100

    Add login page

---
+++ mygit.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ios :: sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+//Add your code here
+int n;
+string s;
+cin>>s>>n;
+if ((s=="<")and(n==3)){
+    cout<<"i love you";
+}
+return 0;
+}
---
+++ todekete.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std; int
+main(){
+ios :: sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+//Add your code here
+int n;
+cin>>n;
+if (n==67){
+    cout<<"i hate";
+}
+return 0;
+}
